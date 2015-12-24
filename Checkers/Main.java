6c5c854d734e5e2430b6ecea60cfad4b8a29c2ca
highnet <joaquintelleria@gmail.com>
2015-12-24 01:34:13 +0100

    Update Main.java

--- Checkers/Main.java
+++ Checkers/Main.java
@@ -11,7 +11,6 @@ public class Main extends JFrame {
     public static final int WIDTH = 650;
     public static final int HEIGHT = 650;
 
-
     public Main() {
         System.out.println("Attempting to initialize game window...");
         this.initialize();
@@ -36,16 +35,15 @@ public class Main extends JFrame {
             }
         });
 
-
     }
 
     private void initialize() {
         this.setSize(WIDTH, HEIGHT);
-        String versionName = "Checkers v0.1 by Joaquin Telleria)";
+        String versionName = "Checkers v0.2 by Joaquin Telleria";
         this.setTitle(versionName);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //      setLocationRelativeTo(null);
-        setLayout(new GridLayout(1, 4)); // Sucks D.
+        setLayout(new GridLayout(1, 1)); // Sucks D.
         this.setVisible(true);
         System.out.println("Succesfully initialized game window...");
     }
