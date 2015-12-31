3495e2cede9bceac7e3ba6df3cd063d3ce170304
highnet <joaquintelleria@gmail.com>
2015-12-31 13:25:37 +0100

    Update GameEngine.java

--- Risk/GameEngine.java
+++ Risk/GameEngine.java
@@ -21,7 +21,21 @@ public class GameEngine extends JPanel implements MouseListener, MouseMotionList
     PlayField gameData;
 
     Font currentlySelectedLabelFont = new Font("Consola", Font.BOLD, 23);
-    Font territoryArmyCountLabelFont = new Font("Bell MT", Font.ITALIC, 18);
+    Font territoryArmyCountLabelFont = new Font("Consola", Font.ITALIC, 18);
+    Font turnNumberLabelFont = new Font("Consola", Font.BOLD, 22);
+    Font newTurnBoxInfoLabelFont = new Font ("Consola", Font.BOLD, 36);
+
+    Gamer humanPlayer1 = new Gamer(Color.blue, true);
+    Gamer computerPlayer1 = new Gamer(Color.red, false);
+
+
+    int turnNumber = 0;
+    boolean newTurn = false;
+    boolean paintNewTurnBox = false;
+    int newTurnBoxTickTimer = 0;
+
+    boolean reinforceMentPhase = false;
+    boolean attackPhase = false;
 
 
     public GameEngine() {
@@ -59,9 +73,16 @@ public class GameEngine extends JPanel implements MouseListener, MouseMotionList
 
         }
 
-        // Prints the outline of all territories
-        for (int i = 0; i < gameData.territory.size(); i++) {
-            gameData.territory.get(i).printTerritory(g, "outline", currentlySelectedName, Color.black);
+
+        // Draw all territories owned by human player
+        for (int i = 0; i < humanPlayer1.myTerritory.size(); i++) {
+            humanPlayer1.myTerritory.get(i).printTerritory(g, "fill", "", humanPlayer1.color);
+        }
+
+
+        // Draw all territories owned by PC player
+        for (int i = 0; i < computerPlayer1.myTerritory.size(); i++) {
+            computerPlayer1.myTerritory.get(i).printTerritory(g, "fill", "", computerPlayer1.color);
         }
 
         // Print all capital cities in black.
@@ -71,9 +92,14 @@ public class GameEngine extends JPanel implements MouseListener, MouseMotionList
 
         // Draws the number of units of each territory.
         for (int i = 0; i < gameData.territory.size(); i++) {
-            g.setColor(Color.white);
+            g.setColor(Color.black);
             g.setFont(territoryArmyCountLabelFont);
             g.drawString(" " + gameData.territory.get(i).getArmyCount(), (int) gameData.territory.get(i).getCapital().getX(), (int) gameData.territory.get(i).getCapital().getY());
+        }
+
+        // Prints the outline of all territories
+        for (int i = 0; i < gameData.territory.size(); i++) {
+            gameData.territory.get(i).printTerritory(g, "outline", currentlySelectedName, Color.black);
         }
 
         // Changes the color of the capital city of the neighbours of currentlyselected to display to the user
@@ -86,9 +112,39 @@ public class GameEngine extends JPanel implements MouseListener, MouseMotionList
 
         }
 
+
         paintCurrentlySelectedLabel(g);
 
+        paintTurnNumberLabel(g);
+        if (newTurn) {
+            paintGraphicsNewTurnBox(g);
+        }
 
+
+    }
+
+    private void paintGraphicsNewTurnBox(Graphics g) {
+
+        if (newTurnBoxTickTimer < 100) {
+            g.setColor(Color.lightGray);
+            g.fillRect(0, 0, 1650, 650);
+            g.setFont(newTurnBoxInfoLabelFont);
+            g.setColor(Color.white);
+            g.drawString("computerplayer 1 - Your Turn", 100, 160);
+
+        }
+        if (newTurnBoxTickTimer == 101) {
+            newTurnBoxTickTimer = 0;
+            paintNewTurnBox = false;
+            newTurn = false;
+        }
+
+    }
+
+    private void paintTurnNumberLabel(Graphics g) {
+
+        g.setFont(turnNumberLabelFont);
+        g.drawString("Turn: " + turnNumber, 910, 25);
     }
 
     private void paintCurrentlySelectedCapitalContourHighlighted(Graphics g) {
@@ -153,9 +209,21 @@ public class GameEngine extends JPanel implements MouseListener, MouseMotionList
     public void actionPerformed(ActionEvent e) {
 
         if (e.getSource() == timer) {
+
+            checkIfNewTurn();
+            if (paintNewTurnBox) {
+                newTurnBoxTickTimer++;
+            }
+
             repaint();
         }
 
+    }
+
+    private void checkIfNewTurn() {
+        if (newTurn) {
+            paintNewTurnBox = true;
+        }
     }
 
     @Override
@@ -167,18 +235,54 @@ public class GameEngine extends JPanel implements MouseListener, MouseMotionList
 
         System.out.println("Click:" + x + "," + y);
 
-        // if currentlyselected == null
-        // if click polygon -> currentlyselected
-
-
         for (int i = 0; i < gameData.territory.size(); i++) {
             if (gameData.territory.get(i).check_isInsideTerritory(x, y)) {
                 System.out.println("Clicked on " + gameData.territory.get(i).getName());
                 currentlySelected = gameData.territory.get(i);
                 currentlySelectedName = currentlySelected.getName();
-                currentlySelected.addArmy(1);
+
                 break;
             }
+        }
+
+
+        if (turnNumber == 0) {
+
+
+            if (currentlySelected != null && !currentlySelected.alreadyOccupied(humanPlayer1, computerPlayer1)) {
+                humanPlayer1.captureTerritory(currentlySelected);
+
+                boolean reAttempt = true;
+
+                while (reAttempt) {
+                    int randomTerritoryFinder = (int) (Math.random() * 41 - 1) + 1;
+                    Territory randomTerritory = gameData.territory.get(randomTerritoryFinder);
+                    if (!randomTerritory.alreadyOccupied(humanPlayer1, computerPlayer1)) {
+                        computerPlayer1.captureTerritory(randomTerritory);
+                        reAttempt = false;
+
+
+                    }
+
+
+                }
+
+            }
+
+            boolean unclaimedTerritoriesExist = false;
+            for (int i = 0; i < gameData.territory.size(); i++) { // iterate through all territories
+                // if any territory is still unclaimed, continue, if all territories are claimed. end turn 0.
+                if (!gameData.territory.get(i).alreadyOccupied(humanPlayer1, computerPlayer1)) {
+                    unclaimedTerritoriesExist = true;
+                }
+            }
+
+            if (!unclaimedTerritoriesExist) {
+                turnNumber++;
+                newTurn = true;
+            }
+
+
         }
 
 
