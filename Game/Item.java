d3e1f93f02fde543d08293d1bec7759a49986350
highnet <joaquintelleria@gmail.com>
2016-04-02 22:04:25 +0200

    Create Item.java

---
+++ Game/Item.java
@@ -0,0 +1,26 @@
+/**
+ * Created by bokense on 26-Mar-16.
+ */
+public class Item {
+
+
+    /*
+
+    ID : 0 : EMPTY
+    ID : 1 : LUMBER
+    ID : 2 : COBBLESTONE
+    ID : 3 : SAND
+
+     */
+
+
+    int ID;
+
+    public Item(int setID){
+        this.ID = setID;
+    }
+
+    public Item(){
+        this.ID = 0;
+    }
+}
