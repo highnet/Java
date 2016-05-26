815dca6f8a448a6baa511677a03f33db95dad392
highnet <joaquintelleria@gmail.com>
2016-05-27 01:29:41 +0200

    Create Main.java

---
+++ gToonsRemake/Main.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileNotFoundException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Main extends JFrame {
+    public final static int WIDTH = 1024;
+    public final static int HEIGHT = 768;
+
+    public Main() {
+        this.initialize();
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        EventQueue.invokeLater(() -> {
+
+
+
+
+            Main window = new Main();
+
+            window.setSize(WIDTH, HEIGHT);
+
+            GameEngine game = new GameEngine();
+
+            window.add(game);
+
+
+        });
+
+
+    }
+
+    private void initialize() {
+
+        this.setSize(WIDTH, HEIGHT);
+        this.setTitle("Gtoons Remake v 0.1 Development");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(1, 1));
+        this.setVisible(true);
+        this.setResizable(false);
+
+    }
+
+
+}
