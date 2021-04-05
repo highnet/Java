c8d6942cc8bde981e47ceb65b571f0c616ad00a1
joaquin telleria <joaquintelleria@gmail.com>
2021-04-05 14:51:34 +0200

    Create StudentSolutionImplementation.java

---
+++ AlgoDat2021/P2/StudentSolutionImplementation.java
@@ -0,0 +1,39 @@
+package main.java.exercise;
+
+import main.java.exercise.graph.Graph;
+import main.java.exercise.helper.Heuristic;
+import main.java.exercise.helper.Point;
+import main.java.exercise.helper.PriorityQueue;
+import main.java.framework.StudentInformation;
+import main.java.framework.StudentSolution;
+
+public class StudentSolutionImplementation implements StudentSolution {
+    @Override
+    public StudentInformation provideStudentInformation() {
+        return new StudentInformation(
+                "Joaquin", // Vorname
+                "Telleria", // Nachname
+                "01408189" // Matrikelnummer
+        );
+    }
+
+    // Implementieren Sie hier Ihre Lösung für die euklidische Distanz
+    public double heuristicManhattanDistance(Point point1, Point point2) {
+        return 0.0;
+        
+    }
+
+    // Implementieren Sie hier Ihre Lösung für die euklidische Distanz
+    public double heuristicEuclideanDistance(Point point1, Point point2) {
+        return 0.0;
+    }
+
+    // Implementieren Sie hier Ihre Lösung für A*
+    public void aStar(Graph g, PriorityQueue q, Heuristic h, int source, int target, int[] path) {
+
+
+
+    }
+
+
+}
